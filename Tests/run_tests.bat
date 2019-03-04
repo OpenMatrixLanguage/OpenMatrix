edb2b0f873d1989192b37bdfc6dacf0b0eae8a90
banspach17 <banspach@altair.com>
2019-03-04 14:42:50 -0500

    Update testing for AppVeyor to show differences in failed core tests

---
+++ Tests/run_tests.bat
@@ -0,0 +1,36 @@
+@echo off
+echo Run tests
+
+call :run_core_test
+goto :end
+
+:run_core_test 
+echo ==================== ====================  ==================== ====================
+echo Run core regression tests
+cd RegressionTests
+echo.
+pwd
+%OML_ROOT%/Tests/regressOMLConsole.pl
+echo Completed core tests
+echo ==================== ====================  ==================== ====================
+if not exist *.log echo ALL core tests PASS! & echo. & goto :EOF
+echo.
+for %%a in ("*.log") do call :run_log_compare %%a
+goto :EOF
+
+:run_log_compare
+echo Display %1 and reflogs\%1 files
+echo -------------------- -------------------- -------------------- 
+echo %1
+echo -------------------- -------------------- -------------------- 
+type %1
+echo -------------------- -------------------- -------------------- 
+echo.
+echo reflogs\%1
+echo -------------------- -------------------- -------------------- 
+type reflogs\%1
+echo -------------------- -------------------- -------------------- 
+echo.
+goto :EOF
+
+:end
