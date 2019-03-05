ff7ba95aecdd2e550501049a79a390c42d7861ec
banspach17 <banspach@altair.com>
2019-03-05 14:36:33 -0500

    Update Travis to show test log differences for core RegressionTests

---
+++ Tests/run_tests.sh
@@ -0,0 +1,34 @@
+run_test () {
+echo ==================== ====================  ==================== ====================
+echo Run Core RegressionTests
+cd $OML_ROOT/Tests/RegressionTests
+echo
+pwd
+# update for travis
+perl $OML_ROOT/Tests/regressOMLConsole.pl -travis
+echo Completed core regression tests
+echo ==================== ====================  ==================== ====================
+display_files "*.log"
+echo " "
+echo Completed Core RegressionTests Log Display
+echo ==================== ====================  ==================== ====================
+}
+
+display_files() {
+shopt -s nullglob
+for f in $1; 
+  do 
+    echo " "; 
+    echo "Display $f file.."; 
+    echo "--------------------"; 
+    cat $f; 
+    echo "--------------------"; 
+    echo " "; 
+    echo "Display reflogs/$f file.."; 
+    echo "--------------------"; 
+    cat reflogs/$f; 
+    echo "--------------------"; 
+  done
+}
+
+run_test
