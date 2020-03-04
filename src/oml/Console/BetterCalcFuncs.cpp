90a69747e350622dea51c636d478abb163e2ce95
banspach17 <banspach@altair.com>
2020-03-03 20:30:05 -0500

    Updates for openmatrix 1.0.4

--- src/oml/Console/BetterCalcFuncs.cpp
+++ src/oml/Console/BetterCalcFuncs.cpp
@@ -37,7 +37,7 @@ extern Interpreter*    interp;
 extern ConsoleWrapper* wrapper;
 
 #define OML_PRODUCT "OpenMatrix "
-#define OML_VERSION "1.0"
+#include "OpenMatrix_Version.h"
 // End defines/includes
 
 #if OS_WIN
