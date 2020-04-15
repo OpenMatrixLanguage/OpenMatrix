7b2c86c848f940e1e82c0fb73f20f850a9efd8fd
banspach17 <banspach@altair.com>
2020-04-15 19:10:31 -0400

    Update for help and other files

--- help/win/en/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ help/win/en/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20190722015849", "oxygen-webhelp/search/index-1.js?uniqueId=20190722015849", "oxygen-webhelp/search/index-2.js?uniqueId=20190722015849", "oxygen-webhelp/search/index-3.js?uniqueId=20190722015849"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20200414060721", "oxygen-webhelp/search/index-1.js?uniqueId=20200414060721", "oxygen-webhelp/search/index-2.js?uniqueId=20200414060721", "oxygen-webhelp/search/index-3.js?uniqueId=20200414060721"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
