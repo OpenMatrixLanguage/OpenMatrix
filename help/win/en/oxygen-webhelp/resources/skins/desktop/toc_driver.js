c02458cc63ff12c35e7616b2b1b3c17b89271f79
Bill Anspach <banspach@altair.com>
2021-03-03 15:50:33 -0500

    Update help files for OpenMatrix 1.0.7

--- help/win/en/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ help/win/en/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20201209100944", "oxygen-webhelp/search/index-1.js?uniqueId=20201209100944", "oxygen-webhelp/search/index-2.js?uniqueId=20201209100944", "oxygen-webhelp/search/index-3.js?uniqueId=20201209100944"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20210219042852", "oxygen-webhelp/search/index-1.js?uniqueId=20210219042852", "oxygen-webhelp/search/index-2.js?uniqueId=20210219042852", "oxygen-webhelp/search/index-3.js?uniqueId=20210219042852"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
